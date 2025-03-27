a6b556364b23acf9438eb3f6897a5b3c51c49f65
tharbouch <tahaharbouch1@gmail.com>
2025-03-27 12:36:01 +0000

    ProductCard test all passed (no styling)

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,3 +1,33 @@
-<template></template>
+<template>
+  <div>
+    <img :src="product.image" alt="product.name" />
+    <h1>{{ product.name }}</h1>
+    <p>{{ product.description }}</p>
+    <p>${{ product.price }}</p>
+    <p>{{ product.quantity }}</p>
+    <p>{{ product.rating }}</p>
+    <button @click="addToCart">Add to Cart</button>
+  </div>
+</template>
 
-<script></script>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProductCard",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["add-to-cart"],
+  setup(props, { emit }) {
+    return {
+      addToCart() {
+        emit("add-to-cart", props.product);
+      },
+    };
+  },
+});
+</script>
